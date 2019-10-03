2857e7b69ad7d77c04133726cce8357e42449d91
ninevra <ninevra@gmail.com>
2019-10-02 17:38:08 -0700

    Reorder and document

--- src/background.js
+++ src/background.js
@@ -1,48 +1,75 @@
+// Register event handlers
 browser.tabs.onRemoved.addListener(handleTabRemoved);
 browser.tabs.onCreated.addListener(handleTabCreated);
-browser.browserAction.onClicked.addListener(newtab);
+browser.browserAction.onClicked.addListener(handleBrowserAction);
 browser.runtime.onStartup.addListener(handleStartup);
 browser.runtime.onInstalled.addListener(handleInstalled);
 
+// Globals:
+
 // A Map whose keys are the cookieStoreIds of all containers managed here
 // and whose values are Sets of all tabIds open in the container
-let containers = new Map();
+const containers = new Map();
 // A Map from tabIds to cookieStoreIds of all tabs managed by this extension
-let tabs = new Map();
-
+const tabs = new Map();
 // A utf-8 TextEncoder
-let utf8Encoder = new TextEncoder();
+const utf8Encoder = new TextEncoder();
+// An array of all names of allowed container colors
+const colors = [
+    "blue", "turquoise", "green", "yellow", "orange", "red", "pink", "purple"
+];
 
+// Event handlers:
+
+// Handles browser starting with the extension installed
+// TODO: verify this is called correctly
 function handleStartup () {
   rebuildDatabase();
 }
 
+// Handles extension being installed, reinstalled, or updated
 function handleInstalled (details) {
   // TODO if this is an update, does the data still exist?
   rebuildDatabase();
 }
 
-async function truncatedHash(string) {
-  // TODO: Does endianness matter here? Can or should DataView be used instead
-  // of Uint8Array?
-
-  // TODO: possibly observed a 3-byte output.  Is this replicable?
-  let buffer = utf8Encoder.encode(string);
-  let hashBuffer = await crypto.subtle.digest('SHA-1', buffer);
-
-  let bytes = new Uint8Array(hashBuffer).slice(0, 4);
-  let hexBytes = new Array(...bytes).map(i => i.toString(16).padStart(2, '0'));
-  return hexBytes.join('');
+// Handles tabs being opened
+// If the tab belongs to a recorded container, then records the tab
+function handleTabCreated (tab) {
+  let cookieStoreId = tab.cookieStoreId;
+  if (containers.has(cookieStoreId)) {
+    addTabToDb(tab);
+  }
 }
 
-let colors = [
-    "blue", "turquoise", "green", "yellow", "orange", "red", "pink", "purple"
-];
-
-function randomChoice (...options) {
-  return options[Math.floor(Math.random() * options.length)];
+// Handles tabs being closed
+// If the tab was recorded, then forget it
+// If the tab's container is now empty, then forget and destroy it
+async function handleTabRemoved (tabId, removeInfo) {
+  // TODO: handle unexpected cases where container not recorded or doesn't
+  // record the tab
+  if (tabs.has(tabId)) {
+    let cookieStoreId = tabs.get(tabId);
+    forgetTab(tabId, cookieStoreId);
+    if (isEmptyContainer(cookieStoreId)) {
+      await forgetAndRemoveContainer(cookieStoreId);
+    }
+  }
 }
 
+// Handles the browserAction being clicked
+// Create a new container and an empty tab in that container
+async function handleBrowserAction (activeTab) {
+  let container = await createContainer();
+  let tab = await browser.tabs.create({
+    cookieStoreId: container.cookieStoreId
+  });
+}
+
+// Core / Consistency:
+// TODO: this is approximately th opposite of a consistency model
+
+// Creates, records, and returns a new temporary container
 async function createContainer () {
   performance.mark("start createContainer");
   let color = randomChoice(...colors)
@@ -56,6 +83,7 @@ async function createContainer () {
     .then(name => browser.contextualIdentities.update(cookieStoreId, {
       name: name
     }));
+  addContainerToDb(container.cookieStoreId);
   performance.mark("end createContainer");
   performance.measure("measure createContainer", "start createContainer", "end createContainer");
   let entry = performance.getEntriesByName("measure createContainer", "measure")[0];
@@ -63,15 +91,8 @@ async function createContainer () {
   return container;
 }
 
-async function genName (container) {
-  return "Temp " + await truncatedHash(container.cookieStoreId + container.color);
-}
-
-async function isManagedContainer (container) {
-  return (container.name.startsWith("Temp") && container.icon == "chill"
-    && container.name == await genName(container));
-}
-
+// Iterates through all containers and tabs to rebuild extension state
+// TODO: inefficient if tabs.query is O(n) in total number of tabs
 async function rebuildDatabase () {
   performance.mark("start rebuild");
   // TODO: if this takes awhile, the results could be inconsistent, because
@@ -104,19 +125,17 @@ async function rebuildDatabase () {
   console.log("Rebuilt database is", containers, tabs);
 }
 
+// State Operations:
+
+// Records a tab in the extension state
+// TODO: handle case where tab is not in a known container
 function addTabToDb (tab) {
   console.log("Recording tab", tab.id, "in container", tab.cookieStoreId);
   tabs.set(tab.id, tab.cookieStoreId);
   containers.get(tab.cookieStoreId).add(tab.id);
 }
 
-function handleTabCreated (tab) {
-  let cookieStoreId = tab.cookieStoreId;
-  if (containers.has(cookieStoreId)) {
-    addTabToDb(tab);
-  }
-}
-
+// Records a container in the extension state
 function addContainerToDb (cookieStoreId) {
   console.log("Recording temporary container: ", cookieStoreId)
   // TODO: this check should always be true
@@ -125,12 +144,15 @@ function addContainerToDb (cookieStoreId) {
   }
 }
 
+// Forgets a tab from the extension state
+// TODO: handle case where tab is not in a known container
 function forgetTab (tabId, cookieStoreId) {
   console.log("Forgetting tab", tabId, "in container", cookieStoreId);
   tabs.delete(tabId);
   containers.get(cookieStoreId).delete(tabId);
 }
 
+// Checks whether a container is believed to be empty
 function isEmptyContainer(cookieStoreId) {
   console.log("Checking status of container", cookieStoreId);
   let containerTabs = containers.get(cookieStoreId);
@@ -138,18 +160,7 @@ function isEmptyContainer(cookieStoreId) {
   return containerTabs.size == 0;
 }
 
-async function handleTabRemoved (tabId, removeInfo) {
-  // TODO: handle unexpected cases where container not recorded or doesn't
-  // record the tab
-  if (tabs.has(tabId)) {
-    let cookieStoreId = tabs.get(tabId);
-    forgetTab(tabId, cookieStoreId);
-    if (isEmptyContainer(cookieStoreId)) {
-      await forgetAndRemoveContainer(cookieStoreId);
-    }
-  }
-}
-
+// Forgets a container from the extension state and destroys it
 async function forgetAndRemoveContainer (cookieStoreId) {
   containers.delete(cookieStoreId);
   await browser.contextualIdentities.remove(cookieStoreId);
@@ -158,11 +169,37 @@ async function forgetAndRemoveContainer (cookieStoreId) {
   // of // the last tab removed. Is this a problem? Is it avoidable?
 }
 
-async function newtab (event) {
-  let container = await createContainer();
-  addContainerToDb(container.cookieStoreId);
-  let tab = await browser.tabs.create({
-    cookieStoreId: container.cookieStoreId
-  });
-  console.log("Created new container", container.cookieStoreId, "and tab", tab.id);
+// Container Utilities:
+
+// Generates a name for the given container
+// Name incorporates a hash of the container's cookieStoreId and color
+async function genName (container) {
+  return "Temp " + await truncatedHash(container.cookieStoreId + container.color, 4);
+}
+
+// Checks whether a container's name and icon are consistent with containers
+// produced by createContainer.  Any such container is considered managed by
+// this extension, and will be deleted when empty.
+async function isManagedContainer (container) {
+  return (container.name.startsWith("Temp") && container.icon == "chill"
+    && container.name == await genName(container));
+}
+
+// Other Utiilities:
+
+// Computes a hexadecimal string hash of the given string
+// Takes the first bytes of the sha1 digest
+async function truncatedHash(string, length) {
+  // TODO: Does endianness matter here? Can or should DataView be used instead
+  // of Uint8Array?
+  let buffer = utf8Encoder.encode(string);
+  let hashBuffer = await crypto.subtle.digest('SHA-1', buffer);
+  let bytes = new Uint8Array(hashBuffer).slice(0, length);
+  let hexBytes = new Array(...bytes).map(i => i.toString(16).padStart(2, '0'));
+  return hexBytes.join('');
+}
+
+// Returns one of its arguments, chosen at random
+function randomChoice (...options) {
+  return options[Math.floor(Math.random() * options.length)];
 }
