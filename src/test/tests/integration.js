6cedca1abc78f4f359a9f7bab300b2b783729273
ninevra <ninevra@gmail.com>
2021-04-07 13:59:13 -0700

    Actually execute tests as esm
    Accidentally forgot to import tests/integration.js as a module. Adds
    type="module" to that, and defers runner.js so that it still runs after
    loading the tests.

--- src/test/tests/integration.js
+++ src/test/tests/integration.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+export {}; // Indicate that this is an ecmascript module
+
 describe('integration tests', () => {
   const background = browser.extension.getBackgroundPage();
   const app = background.app;
